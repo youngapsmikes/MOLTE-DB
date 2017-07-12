426651958f9b395c028ef67b33c0afa4a02639ca
youngapsmikes <michael.yifan.li@gmail.com>
2017-07-11 21:53:18 -0400

    Add files via upload

--- BAKF.m
+++ BAKF.m
@@ -23,35 +23,3 @@ function [beta1,v1,var1,lambda1] = updateStats(theta, prevtheta, nu, beta, v, la
     lambda1 = (1 - a)*lambda + (a)^2;
     
 end 
-
-% function [alpha = BAKF(x, xprev, beta, v, var, lambda)%BAKF(var, bias, n, alpha, lambda)
-% % bias-adjusted Kalman filter stepsize rule
-% 
-% % sigma2 is the variance of the observation theta at timestep n
-% % beta (at n) is the bias computed after iteration n
-% % alpha is the previous stepsize 
-%     
-%     if n == 1 || n == 2 
-%          alpha = 1 / (n+1);
-%     end 
-%     
-%     
-%     
-%     % update stepsizes for next iteration
-%     updateStats(x, xprev, 0.1, beta, v, lambda);
-%     
-%     alpha = 1 - ((var) / ((1+lambda)*var + bias*bias)); 
-% %     else 
-%     lambda = (1-alpha)*(1-alpha)*lambda + alpha*alpha;
-% %     end 
-%     
-% end
-% 
-% function [epsilon, beta,v, var] = updateStats(theta, prevtheta, nu, beta, v, lambda)
-% 
-%     epsilon = prevtheta - theta;
-%     beta = (1-nu)*beta + nu*epsilon;
-%     v = (1-nu)*v + nu*epsilon*epsilon;
-%     var = (v-beta*beta) / (1+lambda);
-%     
-% end 
